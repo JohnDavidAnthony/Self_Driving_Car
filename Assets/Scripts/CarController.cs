c7c15565c580835a4a71e9c29061e646210a8543
John Anthony <johndavid.anthony1@gmail.com>
2019-04-14 17:47:40 -0400

    Removed Unity Agent Stuff starting from scratch

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarController : MonoBehaviour
+{
+
+    // Car properties
+    float acceleration = 15f;
+    float deacceleration = 10f;
+    float turnSpeed = 200f;
+    // The type of drift we are used to 
+    float driftSpeedMoving = .9f;
+    // How fast we drift when we let off the gas and are headed sideways
+    float driftSpeedStatic = .9f;
+
+    // Max amount that we can slide sideways when stopping
+    float maxSideways = .5f;
+    // Start is called before the first frame update
+
+    public Rigidbody2D car;
+
+    // The Cars Sensors
+    public CarSensors frontSensor;
+    public CarSensors frontLeftSensor;
+    public CarSensors frontRightSensor;
+    public CarSensors leftSensor;
+    public CarSensors rightSensor;
+    public bool hitCheckPoint = false;
+
+    float torqueForce = 0;
+    public float AITurn = 1;
+
+    public float carDrive = 0;
+    public float carBrake = 0;
+    public float carTurn = 0;
+
+    public bool playerStopped = false;
+    public bool playerHitWall = false;
+    public bool playerHitCheckPoint = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    float timeLeft = 0;
+    bool timerStarted = false;
+    void FixedUpdate()
+    {
+        car = GetComponent<Rigidbody2D>();
+
+
+        float delay = 5f;
+        // Check to see if car hasnt moved
+        if (car.velocity.magnitude < .05f){
+            // If timer is already going add to it
+            if (timerStarted){
+                timeLeft += Time.deltaTime;
+                if (timeLeft > delay){
+                    // Been idle for to long
+                    Debug.Log("Player Stopped Moving");
+                    playerStopped = true;
+                    timerStarted = false;
+                    timeLeft = 0;
+                }
+            }
+            // Otherwise start timer
+            else{
+                timerStarted = true;
+            }
+        }
+
+        // How fast we drift
+        float driftFactor = driftSpeedStatic;
+        if (ForwardVelocity().magnitude > maxSideways){
+            driftFactor = driftSpeedMoving;
+        }
+        // Reduce sideways velocity from previous inertia using the drift speed
+        car.velocity = ForwardVelocity() + SideVelocity() * driftFactor;
+
+        // Movement
+        if (Input.GetKey(KeyCode.W) || carDrive > 0){
+            // Go forward
+            car.AddForce(transform.up * acceleration);
+
+        }
+        if (Input.GetKey(KeyCode.S) || carBrake > 0){
+            // Go Backwards
+            car.velocity = car.velocity * .99f;
+        }
+
+        // Turning
+        // Don't let car turn if stopped
+        torqueForce = Mathf.Lerp(0, turnSpeed, car.velocity.magnitude / 2);
+        car.angularVelocity = Input.GetAxis("Horizontal") * torqueForce;
+        //car.angularVelocity = carTurn * torqueForce;
+    }
+
+    // Returns our velocity on the forward direction
+    Vector2 ForwardVelocity(){
+        // Out of our velocity, how much is going in the forward direction
+        // Using the dot product of velocity and our forward
+        return transform.up * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.up);
+    }
+
+    // Returns our velocity on the sideways direction
+    Vector2 SideVelocity(){
+        return transform.right * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        //Player collided with wall
+        //collided = true;
+        if (other.gameObject.tag == "CheckPoint"){
+            return;
+        }
+        Debug.Log("Player hit Wall");
+        playerHitWall = true;
+       
+    }
+}
---
+++ Assets/Scripts/CarSensors.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSensors : MonoBehaviour{
+    public Transform car;
+    public Transform rayEnd;
+    public float distance = 10000000f;
+
+    public Text distanceText;
+
+    private void Start(){
+    }
+
+    // Update is called once per frame
+    void LateUpdate(){
+
+        Vector2 direction = rayEnd.position - car.position;
+
+
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask1 = 1 << 8;
+        // The ignore raycast layer
+        int layerMask2 = 1 << 2;
+        // Bit or to combine them
+        int layerMask = layerMask1 | layerMask2;
+        // This would cast rays only against our layer mask.
+        // But instead we want to collide against everything except selected layers
+        layerMask = ~layerMask;
+
+        // Cast the ray
+        RaycastHit2D hit = Physics2D.Raycast(car.position, direction, direction.magnitude, layerMask);
+        if (hit.collider != null){
+            // Check to see if collision is a checkpoint
+
+            distance = hit.distance;
+            Debug.DrawRay(car.position, direction, Color.red);
+
+            distanceText.text = hit.distance.ToString("0.00");
+
+        }else{
+            distance = 10000000f;
+            Debug.DrawRay(car.position, direction, Color.white);
+            distanceText.text = "Null";
+        }
+
+    }
+}
