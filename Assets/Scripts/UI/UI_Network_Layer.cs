e1ca2794ab7051af5d7ede4b72d5ab962eb79347
JD <johndavid.anthony1@gmail.com>
2019-04-27 20:46:21 -0400

    Revert "Revert "temp""
    This reverts commit d223e923ec2f04b7638a799516c55cf20f85c061.

---
+++ Assets/Scripts/UI/UI_Network_Layer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_Network_Layer : MonoBehaviour
+{
+    public RectTransform contents;
+    public List<UI_Network_Layer_Nodes> nodes;
+
+    // Build and Display layer with the nodes
+    public void Display(Layer layer) {
+        Display(layer.NumNeurons());
+    }
+
+    public void Display(int numNeurons){
+        UI_Network_Layer_Nodes nodeTemplate = nodes[0];
+
+        // Create all the nodes for our layer
+        for (int i = 0; i < numNeurons -1; i++)
+        {
+            UI_Network_Layer_Nodes node = Instantiate(nodeTemplate);
+            node.transform.SetParent(contents.transform, false);
+            nodes.Add(node);
+        }
+    }
+
+    public void DisplayConnections(Layer currentLayer, UI_Network_Layer nextLayer, NeuralNetwork network)
+    {
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            nodes[i].DisplayConnections(i, currentLayer, nextLayer, network);
+        }
+            
+    }
+    public void HideAllConnections()
+    {
+        foreach (UI_Network_Layer_Nodes node in nodes)
+            node.HideConnections();
+    }
+}
